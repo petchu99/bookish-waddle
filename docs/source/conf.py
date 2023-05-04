38a558d78c34cfa05c1afa68afea23624e949dbc
petchu99 <131541050+petchu99@users.noreply.github.com>
2023-05-04 10:20:31 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'SamePHY'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, aiMeter'
+author = 'Engr Dibia Peter'
 
 release = '0.1'
 version = '0.1.0'
